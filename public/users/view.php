04e8d4560a64a1c4b6c103b3b98f5a0db27b4159
chad svastisalee <swasgrafx@gmail.com>
2020-10-23 14:02:57 -0500

    Complete User CRUD

---
+++ public/users/view.php
@@ -0,0 +1,35 @@
+<?php
+include '../../core/db_connect.php';
+
+$input = filter_input_array(INPUT_GET);
+$id = !(empty($input['id']))?$input['id']:null;
+$email = !(empty($input['email']))?$input['email']:null;
+
+if(!empty($email)){
+  $lookup = $email;
+  $where = 'email = :lookup';
+}else{
+  $lookup = $id;
+  $where = 'id = :lookup';
+}
+
+$content=null;
+$stmt = $pdo->prepare('SELECT * FROM users WHERE id = ?');
+$stmt->execute([$id]);
+$row = $stmt->fetch();
+
+$meta=[];
+$meta['title']="{$row['first_name']} {$row['last_name']}";
+
+$content=<<<EOT
+<h1>{$row['first_name']} {$row['last_name']}</h1>
+{$row['email']}
+
+<hr>
+<div>
+  <a class="btn btn-link" href="edit.php?id={$row['id']}">Edit</a>
+  <a class="btn btn-link text-danger" href="delete.php?id={$row['id']}">Delete</a>
+</div>
+EOT;
+
+include '../../core/layout.php';
